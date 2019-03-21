81f11eeb98f6575b29aecc2820e44b65bc0237c7
litsora <s0ralit@protonmail.com>
2019-03-21 20:45:25 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      post 'users' => 'user#signup'
+      post 'signin' => 'user#signin'
+      post 'image_regist' => 'user#image_regist'
+      get 'users' => 'user#show'
+      patch 'users' => 'user#update'
+      delete 'users' => 'user#delete'
+      post 'Judgment' => 'judgement#show'
+    end
+  end
 end
