9a23072f1f9136c11347055ae3be945cb847124e
litsora <s0ralit@protonmail.com>
2019-03-24 13:35:00 +0900

    Add verification user function

---
+++ app/services/firebase.rb
@@ -0,0 +1,10 @@
+require 'httparty'
+class Firebase
+  def verification_user_by_id_token(id_token)
+    base_url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo?key=#{ENV['firebase_token']}".freeze
+    HTTParty.post(
+                base_url,
+                :body => {:idToken => id_token}
+    )
+  end
+end
