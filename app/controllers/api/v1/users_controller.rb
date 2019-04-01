fd7f2787b26663ffa24dfa8ff36b4d28a38f7764
litsora <s0ralit@protonmail.com>
2019-04-01 19:45:57 +0900

    Add Verification API Key and Secret and Change the logic to Verification API Key and Secret

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,35 +4,39 @@ require './app/services/firebase'
 
 class Api::V1::UsersController < Api::ApplicationController
   def signup
-    firebase = FirebaseClient.new
-    res = firebase.verification_user_by_id_token(params[:id_token])
-    if res.code == 200
-      if User.find_by(id_token: params[:id_token])
-        # 既にこのid_tokenを保有するUserが存在する場合
-        response_unauthorized
-      else
-        if (ENV[:API_KEY] == request.header[:HTTP_API_KEY]) && (ENV[:API_SECRET] == request.header[:HTTP_API_SECRET])
+    if (ENV[:API_KEY] == request.header[:HTTP_API_KEY]) && (ENV[:API_SECRET] == request.header[:HTTP_API_SECRET])
+      firebase = FirebaseClient.new
+      res = firebase.verification_user_by_id_token(params[:id_token])
+      if res.code == 200
+        if User.find_by(id_token: params[:id_token])
+          # 既にこのid_tokenを保有するUserが存在する場合
+          response_unauthorized
+        else
           @user = User.new(user_params)
           payload = TokenProvider.refresh_tokens @user
           response_success('signup', 'create', payload)
-        else
-          responce_forbidden
         end
+      else
+        response_bad_request
       end
     else
-      response_bad_request
+      responce_forbidden
     end
   end
 
   def signin
-    firebase = FirebaseClient.new
-    res = firebase.verification_user_by_id_token(params[:id_token])
-    if res.code == 200
-      user = User.find_by(id_token: params[:id_token])
-      tokens = TokenProvider.refresh_tokens user
-      response_success('signin', 'login', tokens)
+    if (ENV[:API_KEY] == request.header[:HTTP_API_KEY]) && (ENV[:API_SECRET] == request.header[:HTTP_API_SECRET])
+      firebase = FirebaseClient.new
+      res = firebase.verification_user_by_id_token(params[:id_token])
+      if res.code == 200
+        user = User.find_by(id_token: params[:id_token])
+        tokens = TokenProvider.refresh_tokens user
+        response_success('signin', 'login', tokens)
+      else
+        response_unauthorized
+      end
     else
-      response_unauthorized
+      responce_forbidden
     end
   end
 
