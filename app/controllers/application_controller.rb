cb2d8beed9f92d8cb6c531ca9729c1de1b71d6ad
litsora <s0ralit@protonmail.com>
2019-04-01 19:18:11 +0900

    Fix Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,8 +36,9 @@ class ApplicationController < ActionController::API
         data: exception.record.errors
     }, status: 422
   end
+
   # 200 Status OK
-  def response_success(class_name, action_name, data=nil)
+  def response_success(class_name, action_name, data = nil)
     if data.nil?
       render status: 200, json: { status: 200, message: "Success #{class_name.capitalize} #{action_name.capitalize}" }
     else
